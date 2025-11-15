6ba7a2151b6c50aa54e19ff7b98559ce4fa45b9c
saklen077 <110762373+saklen077@users.noreply.github.com>
2025-11-15 20:55:53 +0530

    working on backend. now frontend done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route , Routes } from 'react-router-dom'
+import { Route , Routes, useLocation } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
 import ChatBox from './components/ChatBox'
 import Credits from './pages/Credits'
@@ -7,13 +7,21 @@ import Community from './pages/Community'
 import { assets } from './assets/assets'
 import { useState } from 'react'
 import './assets/prism.css'
+import Loading from './pages/Loading'
+import { useAppContext } from './context/AppContext'
+import Login from './pages/Login'
 export default function App() {
 
   const [isMenuOpen , setIsMenuOpen] = useState(false)
+  const {pathname} = useLocation()
+  const {user} = useAppContext()
+  if(pathname === '/loading') return <Loading />
   return (
     <>
     {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert' onClick={()=> setIsMenuOpen(true)}/>}
-    <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+
+    {user ? (
+      <div className='dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white'>
        <div className='flex h-screen w-screen'>
       <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
       <Routes>
@@ -23,6 +31,11 @@ export default function App() {
       </Routes>
     </div>
     </div>
+    ):(
+      <div className='bg-linear-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen'>
+        <Login />
+      </div>
+    )}
     </>
   )
 }
